93fb84b4f67a4c5e97733d9210159fd2524a1e16
aleax <alex.maglio.neyra.herrada@gmail.com>
2025-08-04 20:23:39 -0500

    fix(home): fix mixin state page import route

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -14,7 +14,7 @@ import 'package:flutter_state_managment/components/custom_button.dart';
 import 'package:flutter_state_managment/bloc/pages/bloc_page.dart';
 import 'package:flutter_state_managment/cubit/pages/cubit_page.dart';
 import 'package:flutter_state_managment/flutter_set_state/flutter_set_state_page.dart';
-import 'package:flutter_state_managment/getx_mixin_state/pages/getx_simple_state_page.dart';
+import 'package:flutter_state_managment/getx_mixin_state/pages/getx_mixin_state_page.dart';
 import 'package:flutter_state_managment/getx_reactive_state/pages/getx_reactive_state_page.dart';
 import 'package:flutter_state_managment/getx_simple_state/pages/getx_simple_state_page.dart';
 
